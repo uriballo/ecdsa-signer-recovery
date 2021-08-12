93ab46672485d47f3b0a9b4b976f245c4f23e225
Oriol Balló <56364336+uriballo@users.noreply.github.com>
2021-08-12 08:58:37 +0200

    Material-ui updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { ethers } from 'ethers'
 import ESVT from './artifacts/contracts/ESVT.sol/ESVT.json'
 import '@fontsource/roboto'
+import { Tooltip } from '@material-ui/core';
 
 // Contract address (local)
 //const esvtAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+// GWEI cost 0.000028245
 // Rinkeby address
 const esvtAddress = "0xb330060C65B462ebC71CDeb8C4E81f5B0394C42B"
 
@@ -17,8 +20,15 @@ const useStyles = makeStyles((theme) => ({
     '& > *': {
       margin: theme.spacing(1),
       width: '25ch',
+      maxWidth:500,
     },
   },
+  customWidth: {
+    maxWidth: 500,
+  },
+  fontSize: {
+    fontSize: 15
+  }
 }));
 
 function App() {
@@ -42,7 +52,6 @@ function App() {
       const contract = new ethers.Contract(esvtAddress, ESVT.abi, provider)
 
       const address = await contract.recoverOwner(messageHash, signature)
-      
       setAddress(address)
     }
   }
@@ -62,9 +71,16 @@ function App() {
     }
   }
 
+  function copyToClipboard(clipboardText){
+    console.log(clipboardText);
+    navigator.clipboard.writeText(clipboardText)
+  }
+
   return (
     <div className="esvt">
-      <h1>ESVT</h1>
+      <Typography variant="h1" component="h2" gutterBottom>
+        ESVT
+      </Typography>
       <form>
         <h2 className="label-wrapper">
           <label htmlFor="message" className="label__lg">
@@ -75,11 +91,51 @@ function App() {
         <div>&nbsp; </div>
         <Button variant="contained" type='button' className="btn btn__primary btn__lg" onClick={signMessage}>SIGN</Button>
         <Button variant="contained" type='button' className="btn btn__primary btn__lg" onClick={verifySignature}>VERIFY</Button>
-        <p>Original Message: {message}</p>
-        <p>Message Hash: {messageHash}</p>
-        <p>Signature: {signature}</p>
-        <p>Address: {address}</p>
-        <p>Contract address: {esvtAddress}</p>
+        <div>&nbsp; </div>
+        <Typography variant="h4" gutterBottom>
+          Original Message:
+        </Typography>
+        <Tooltip title={'Click to copy!'} onClick={copyToClipboard(message)} classes={{ tooltip: classes.fontSize }}>
+          <Typography noWrap variant="h5" gutterBottom>
+            {message}
+          </Typography>
+        </Tooltip>
+        <div>&nbsp; </div>
+        <Typography variant="h4" gutterBottom>
+          Message Hash:
+        </Typography>
+        <Tooltip title={'Click to copy!'} onClick={copyToClipboard(messageHash)} classes={{ tooltip: classes.fontSize }}>
+          <Typography noWrap variant="h5" gutterBottom>
+            {messageHash}
+          </Typography>
+        </Tooltip>
+        <div>&nbsp; </div>
+        <Typography variant="h4" gutterBottom>
+          Signature:
+        </Typography>
+        <Tooltip title={'Click to copy!'} onClick={copyToClipboard(signature)} classes={{ tooltip: classes.fontSize }}>
+          <Typography noWrap variant="h5" gutterBottom>
+            {signature}
+          </Typography>
+        </Tooltip>
+        <div>&nbsp; </div>
+        <Typography variant="h4" gutterBottom>
+          Signer Address:
+        </Typography>
+        <Tooltip title={'Click to copy!'} onClick={copyToClipboard(address)} classes={{ tooltip: classes.fontSize }}>
+          <Typography noWrap variant="h5" gutterBottom>
+            {address}
+          </Typography>
+        </Tooltip>
+        <div>&nbsp; </div>
+        <Typography variant="h4" gutterBottom>
+          Contract Address:
+        </Typography>
+        <Tooltip title={'Click to copy!'} onClick={copyToClipboard(esvtAddress)} classes={{ tooltip: classes.fontSize }}>
+          <Typography noWrap variant="h5" gutterBottom>
+            {esvtAddress}
+          </Typography>
+        </Tooltip>
       </form>
     </div>
   );
